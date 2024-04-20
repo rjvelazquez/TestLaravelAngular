30f51d87d76e23e1941f4d20fd1f422bf0fae456
Roberto Velazquez <rochugui10@gmail.com>
2024-04-19 20:01:05 -0400

    Init

---
+++ backend/database/migrations/2024_04_19_225419_create_administradores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('administradores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('apellido', 100);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('administradores');
+    }
+};
---
+++ backend/database/migrations/2024_04_19_225420_create_estudiantes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('apellido', 100)->nullable();
+            $table->integer('edad');
+            $table->string('cedula', 11);
+            $table->string('email')->unique();
+            $table->json('cursos_asociados')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+};
