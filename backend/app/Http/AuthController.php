362326546bde364a919ee0042a184c6233c37a68
Roberto Velazquez <rochugui10@gmail.com>
2024-04-19 22:00:49 -0400

    modify backend

---
+++ backend/app/Http/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+// app/Http/Controllers/AuthController.php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json(['message' => 'Login exitoso'], 200);
+        }
+
+        return response()->json(['error' => 'Credenciales inválidas'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['message' => 'Logout exitoso'], 200);
+    }
+}
---
+++ backend/app/Http/Controllers/Kernel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\Authorize;
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Illuminate\Foundation\Http\Middleware\TrimStrings;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+use Illuminate\Http\Middleware\TrustProxies;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        TrustProxies::class,
+        CheckForMaintenanceMode::class,
+        TrimStrings::class,
+        ConvertEmptyStringsToNull::class,
+    ];
+
+    protected $middlewareGroups = [
+        'web' => [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            VerifyCsrfToken::class,
+            SubstituteBindings::class,
+            EnsureFrontendRequestsAreStateful::class,
+
+        ],
+        'api' => [
+            'throttle:60,1',
+            SubstituteBindings::class,
+        ],
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => Authenticate::class,
+        'can' => Authorize::class,
+        'throttle' => ThrottleRequests::class,
+        'bindings' => SubstituteBindings::class,
+        'frontend' => EnsureFrontendRequestsAreStateful::class,
+    ];
+}
